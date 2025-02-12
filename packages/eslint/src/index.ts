e8a8ec720abac4450bb25be8d60056a106bf839e
Nick Williams <git@nickawilliams.com>
2025-02-12 15:10:05 -0800

    feat(eslint): added eslint defaults

---
+++ packages/eslint/src/index.ts
@@ -0,0 +1,51 @@
+import js from '@eslint/js'
+import type { Linter } from 'eslint'
+import prettier from 'eslint-config-prettier'
+import ts from 'typescript-eslint'
+
+export default ts.config(
+  js.configs.recommended,
+  ts.configs.recommendedTypeChecked,
+  prettier,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+    rules: {
+      '@typescript-eslint/consistent-type-assertions': [
+        'error',
+        {
+          assertionStyle: 'as',
+          objectLiteralTypeAssertions: 'allow',
+        },
+      ],
+      '@typescript-eslint/explicit-module-boundary-types': 'error',
+      '@typescript-eslint/no-inferrable-types': [
+        'error',
+        {
+          ignoreParameters: true,
+          ignoreProperties: true,
+        },
+      ],
+      '@typescript-eslint/no-useless-constructor': 'error',
+      '@typescript-eslint/return-await': 'error',
+      'eol-last': ['error', 'always'],
+    },
+  },
+  {
+    files: ['**/*.test.ts', '**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unsafe-declaration-merging': 'off',
+      '@typescript-eslint/await-thenable': 'off',
+      '@typescript-eslint/unbound-method': 'off',
+    },
+  },
+) as Linter.Config
