8ffcff2a93466bbf5fd28def1b1ae854b6324d69
Nick Williams <git@nickawilliams.com>
2025-02-11 16:30:26 -0800

    feat(prettier): added prettier defaults

---
+++ packages/prettier/src/index.ts
@@ -0,0 +1,17 @@
+import type { Options } from 'prettier'
+
+export default {
+  "arrowParens": "avoid",
+  "plugins": [
+    "prettier-plugin-multiline-arrays",
+    "prettier-plugin-organize-imports",
+    "prettier-plugin-packagejson",
+    "prettier-plugin-sh",
+    "prettier-plugin-terraform-formatter"
+  ],
+  "multilineArraysWrapThreshold": 1,
+  "semi": false,
+  "singleQuote": true,
+  "trailingComma": "all",
+  "printWidth": 80
+} satisfies Options
---
+++ packages/prettier/tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  dts: true,
+  minify: false,
+  sourcemap: true,
+  clean: true,
+  splitting: false,
+  shims: false,
+})
