06ffb8be7f6c82311d9536f14b0fa833bba52553
Nick Williams <git@nickawilliams.com>
2025-02-13 20:15:11 -0800

    chore: updated eslint config to ignore build outputs

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,1 +1,9 @@
-export { default } from '@rogwilco/defaults-eslint'
+import config from '@rogwilco/defaults-eslint'
+import type { Linter } from 'eslint'
+
+export default [
+  {
+    ignores: ['**/dist'],
+  },
+  ...config,
+] satisfies Linter.Config[]
